0146cc9e4360b85920997f75f2e0e304c09b25cb
RENT-Komputer\RENT <marekj.szulc>
2017-10-30 10:33:59 +0100

    zadanie 6

---
+++ src/zadanie6/Main.java
@@ -0,0 +1,45 @@
+package zadanie6;
+
+public class Main {
+    public static void main(String[] args) {
+        int ocenaMatematyka = 3;
+        int ocenaChemia = 5;
+        int ocenaJPolski = 6;
+        int ocenaWos = 1;
+        int ocenaInformatyka = 4;
+
+        int sredniaAll = (ocenaChemia + ocenaInformatyka + ocenaJPolski + ocenaMatematyka + ocenaWos)/5;
+        int sredniaScisle = (ocenaChemia + ocenaInformatyka + ocenaMatematyka)/3;
+        int sredniaHumanistyczne = (ocenaJPolski + ocenaWos)/2;
+
+        System.out.println("srednia ze wszystkiego = " + sredniaAll);
+        System.out.println("srednia ze scislych = " + sredniaScisle);
+        System.out.println("srednia z humanistycznych = " + sredniaHumanistyczne);
+if (ocenaChemia == 1){
+    System.out.println("Pala z chemii");
+}
+if (ocenaInformatyka == 1){
+    System.out.println("Pala z informatyki. WSTYD!");
+}
+if (ocenaJPolski == 1){
+    System.out.println("Pala z polskiego");
+}
+if (ocenaMatematyka == 1){
+    System.out.println("Pala z matematyki! WSTYYYYD");
+}
+if (ocenaWos == 1){
+    System.out.println("Pala z WOS'u");
+}
+if (sredniaAll == 1){
+    System.out.println("srednia z wszystkiego NDST!!");
+}
+if (sredniaHumanistyczne == 1) {
+    System.out.println("paly z humanistycznych");
+}
+if (sredniaScisle == 1){
+    System.out.println("paly ze scislych!");
+}
+
+    }
+
+}
