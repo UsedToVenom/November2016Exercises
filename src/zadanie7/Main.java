27542556f9f22b734b3db22188daa67194c2c322
RENT-Komputer\RENT <marekj.szulc>
2017-10-30 10:37:18 +0100

    zadanie 7

---
+++ src/zadanie7/Main.java
@@ -0,0 +1,9 @@
+package zadanie7;
+
+public class Main {
+    public static void main(String[] args) {
+        double stopnieC = 0;
+        double stopnieF = stopnieC * 1.8 + 32;
+        System.out.println(stopnieF);
+    }
+}
