d9107b86ac5fe7ada2a4b94d3df8cef803de946e
RENT-Komputer\RENT <marekj.szulc>
2017-10-30 15:00:27 +0100

    zadanie 14

--- src/zadanie14/Main.java
+++ src/zadanie14/Main.java
@@ -1,4 +1,14 @@
 package zadanie14;
 
 public class Main {
+    public static void main(String[] args) {
+        System.out.println("straight copy1");
+        for (int i = 65; i <= 90; i++) {
+            System.out.println((char)i);
+        }
+        System.out.println("\nStraight copy2");
+        for (char alphabet = 'Z'; alphabet >= 'A'; alphabet--) {
+            System.out.println(alphabet);
+        }
+    }
 }
