04c47fea94b800c12050bbff9a01a5a889097633
RENT-Komputer\RENT <marekj.szulc>
2017-10-30 09:28:20 +0100

    zadanie 1 i 2

---
+++ src/zadanie2/Main.java
@@ -0,0 +1,15 @@
+package zadanie2;
+
+public class Main {
+    public static void main(String[] args) {
+        int liczba1 = 4;
+        int liczba2 = 6;
+
+        int a;
+        a = liczba1 * liczba2;
+        System.out.println(a);
+    }
+
+
+}
+
