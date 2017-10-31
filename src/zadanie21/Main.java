242b6ed668c4e3e83814e16ea6f427fffae83fcc
RENT-Komputer\RENT <marekj.szulc>
2017-10-31 12:31:23 +0100

    add: zadanie 21

---
+++ src/zadanie21/Main.java
@@ -0,0 +1,37 @@
+package zadanie21;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("okay kochasiu, budujemy choineczkę bo spooky scarry skeletons juz sie konczy, czas na swieta");
+
+        Scanner nasluchiwacz = new Scanner (System.in);
+        System.out.println("podaj wysokosc choinki");
+
+        String liniaWysokosci = nasluchiwacz.nextLine();
+        try{
+            int wysokosc = Integer.parseInt(liniaWysokosci);
+            for (int wiersz = 1; wiersz <= wysokosc; wiersz++){
+                for (int kolumna = 1 ; kolumna <= (2*wysokosc) - 1; kolumna++){
+                    if (wiersz < wysokosc && kolumna <= wysokosc - wiersz){
+                        System.out.print(" ");
+                    }
+                    if (wiersz <= wysokosc && kolumna>= wysokosc - wiersz && kolumna < wiersz + kolumna && kolumna < wysokosc + wiersz-1){
+                        System.out.print("*");
+                    }
+                    if (wiersz == wysokosc && kolumna == (2*wysokosc) - 1){
+                        System.out.print("*");
+                    }
+                    if (kolumna == (2*wysokosc) - 1){
+                        System.out.println(" ");
+                    }
+                }
+            }
+
+        }catch(NumberFormatException nfe){
+            System.out.println("To nie byla liczba oszuscie!");
+        }
+
+    }
+}
---
+++ src/zadanie21/MainVariantAdriana.java
@@ -0,0 +1,26 @@
+package zadanie21;
+import java.util.Scanner;
+
+public class MainVariantAdriana {
+    public static void main(String[] args) {
+        System.out.println("Wpisz wysokosc choinki ");
+        Scanner konsola = new Scanner(System.in);
+        int n = konsola.nextInt();
+
+        int x = n;
+        int y = 1;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < x; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k < y; k++) {
+                System.out.print("x");
+            }
+            x--;
+            y += 2;
+            System.out.println();
+        }
+
+    }
+}
+
