d76d8e88b4bc98687b207b4c9493993158e50d6a
RENT-Komputer\RENT <marekj.szulc>
2017-10-31 10:06:06 +0100

    add: zadanie 20 oraz fix: zadanie 19

---
+++ src/zadanie20/Main.java
@@ -0,0 +1,38 @@
+package zadanie20;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("gramy w za dużo za mało kochasiu. Ja wylosuję ci liczbę 0-100, potem zobacyzmy w ile strzałow uda ci sie ja trafic!");
+
+        int randomNumber = (int) (Math.random() * (100 - 0) + 0);
+
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String commandLine = scanner.nextLine();
+
+            if (commandLine.equals("hack")) {
+                System.out.println(randomNumber);
+            }
+
+            try {
+                int proba = Integer.parseInt(commandLine);
+                if (proba == randomNumber) {
+                    System.out.println("gratulacje! wygrałeś! Popełniam harakiri" + "\nProgram się zabił");
+                    System.exit(0);
+                } else if (proba < randomNumber) {
+                    System.out.println("za malo!");
+                } else if (proba > randomNumber) {
+                    System.out.println("za duzo!");
+                } else {
+                    System.out.println("ERROR");
+                }
+            } catch (NumberFormatException nfe) {
+                System.out.println("To nie byla liczba!");
+            }
+
+        }
+    }
+}
