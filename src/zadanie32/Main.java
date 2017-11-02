3dc55b4a06b97b7094cac4bb9a37823c75cb6195
RENT-Komputer\RENT <marekj.szulc>
2017-11-02 11:33:05 +0100

    add: zadanie33, fix: alt rozwiazanie 32

--- src/zadanie32/Main.java
+++ src/zadanie32/Main.java
@@ -14,15 +14,23 @@ public class Main {
 
         int sumOfDigits = 0;
 
-        String tabela [] = new String [length];
-        for (int i = 0; i<=length - 1; i++){
-            tabela[i] = String.valueOf(lineExpression.charAt(i));
-        }
+//        String tabela [] = new String [length];
+//        for (int i = 0; i<=length - 1; i++){
+//            tabela[i] = String.valueOf(lineExpression.charAt(i));
+//        }
+//        for (int i = 0; i<=length-1; i++){
+//            try{
+//                int liczba = Integer.valueOf(tabela[i]);
+//                sumOfDigits = sumOfDigits + liczba;
+//            }catch(NumberFormatException nfe){}
+//        }
+//        System.out.println("suma cyfr to " + sumOfDigits);
+
         for (int i = 0; i<=length-1; i++){
-            try{
-                int liczba = Integer.valueOf(tabela[i]);
-                sumOfDigits = sumOfDigits + liczba;
-            }catch(NumberFormatException nfe){}
+                if(Character.isDigit(lineExpression.charAt(i))) {
+                    sumOfDigits = sumOfDigits + Character.getNumericValue(lineExpression.charAt(i));//tabela[i] - '0'
+                                                                                        // tabela[i] - 48
+                }
         }
         System.out.println("suma cyfr to " + sumOfDigits);
     }
---
+++ src/zadanie34/Main.java
@@ -0,0 +1,7 @@
+package zadanie34;
+
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
